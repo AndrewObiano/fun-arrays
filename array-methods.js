caec1fdd41770f5eaf17b3355288e9758100fadc
Andrew Obiano <andrew.obiano@gmail.com>
2020-01-04 17:40:18 -1000

    made corrections to final answer

--- array-methods.js
+++ array-methods.js
@@ -5,21 +5,17 @@ var dataset = require("./dataset.json");
   greater than 100000
   assign the resulting new array to `hundredThousandairs`
 */
-var hundredThousandairs = null;
-hundredThousandairs = dataset.bankBalances.filter(antiPoverty);
-
-function antiPoverty(element, index, array) {
+var hundredThousandairs = dataset.bankBalances.filter(function(element) {
   return element.amount > 100000;
-}
+});
 
 // set sumOfBankBalances to be the sum of all value held at `amount` for each bank object
 var sumOfBankBalances = null;
 let allBankAccounts = dataset.bankBalances;
-allBankAccounts.forEach(sum);
 
-function sum(element, index, array) {
+allBankAccounts.forEach(function(element) {
   return (sumOfBankBalances += parseInt(element.amount));
-}
+});
 
 /*
   from each of the following states:
@@ -33,9 +29,8 @@ function sum(element, index, array) {
   and then sum it all up into one value saved to `sumOfInterests`
  */
 var sumOfInterests = null;
-allBankAccounts.reduce(gimmeYoInterest);
 
-function gimmeYoInterest(prev, curr) {
+allBankAccounts.reduce(function(prev, curr) {
   if (
     curr.state === "WI" ||
     curr.state === "IL" ||
@@ -46,7 +41,8 @@ function gimmeYoInterest(prev, curr) {
   ) {
     return (sumOfInterests += Math.round(parseInt(curr.amount) * 0.189));
   }
-}
+});
+
 /*
   aggregate the sum of bankBalance amounts
   grouped by state
@@ -63,11 +59,8 @@ function gimmeYoInterest(prev, curr) {
     round this number to the nearest dollar before moving on.
   )
  */
-var stateSums = null;
-stateSums = allBankAccounts.reduce(byState, {});
-// dont forget reduce(functionName, THIS VALUE IS THE INITIAL STATE OF acc)
-
-function byState(acc, curr) {
+// dont forget reduce(functionName[, THIS VALUE IS THE INITIAL STATE OF acc])
+var stateSums = allBankAccounts.reduce(function(acc, curr) {
   if (acc[curr.state]) {
     // if the state is already in the object
     acc[curr.state] += parseInt(curr.amount);
@@ -76,7 +69,8 @@ function byState(acc, curr) {
     acc[curr.state] = parseInt(curr.amount);
   }
   return acc;
-}
+}, {});
+
 /*
   for all states *NOT* in the following states:
     Wisconsin
@@ -94,10 +88,7 @@ function byState(acc, curr) {
     round this number to the nearest dollar before moving on.
   )
  */
-var sumOfHighInterests = null;
-sumOfHighInterests = Object.entries(stateSums).reduce(ballaInterest, 0);
-
-function ballaInterest(acc, curr) {
+var sumOfHighInterests = Object.entries(stateSums).reduce(function(acc, curr) {
   let interestValue = Math.round(parseInt(curr[1]) * 0.189);
   if (!["WI", "IL", "OH", "WY", "GA", "DE"].includes(curr[0])) {
     // check if current state is one of these
@@ -105,33 +96,30 @@ function ballaInterest(acc, curr) {
       acc += interestValue;
     }
   }
-  console.log(acc);
   return acc;
-}
+}, 0);
+
 /*
   set `lowerSumStates` to be an array of two letter state
   abbreviations of each state where the sum of amounts
   in the state is less than 1,000,000
  */
-var lowerSumStates = null;
-lowerSumStates = Object.entries(stateSums).reduce(toPoorStates, []);
-
-function toPoorStates(acc, curr) {
+var lowerSumStates = Object.entries(stateSums).reduce(function(acc, curr) {
   if (curr[1] < 1000000) {
     acc.push(curr[0]);
   }
   return acc;
-}
+}, []);
+
 /*
   aggregate the sum of each state into one hash table
   `higherStateSums` should be the sum of all states with totals greater than 1,000,000
  */
-var higherStateSums = null;
-higherStateSums = Object.entries(stateSums)
-  .reduce(moreBalla, [])
+var higherStateSums = Object.entries(stateSums)
+  .reduce(moreRich, [])
   .reduce(total, 0);
 
-function moreBalla(acc, curr) {
+function moreRich(acc, curr) {
   if (curr[1] > 1000000) {
     acc.push(curr[1]); // push the total
   }
@@ -156,10 +144,9 @@ function total(acc, curr) {
   if true set `areStatesInHigherStateSum` to `true`
   otherwise set it to `false`
  */
-var areStatesInHigherStateSum = null;
-areStatesInHigherStateSum = Object.entries(stateSums).every(topDollaCheck);
+var areStatesInHigherStateSum = Object.entries(stateSums).every(topDollarCheck);
 
-function topDollaCheck(element) {
+function topDollarCheck(element) {
   if (!["WI", "IL", "OH", "WY", "GA", "DE"].includes(element[0])) {
     return element[1] > 2550000;
   }
@@ -178,8 +165,7 @@ function topDollaCheck(element) {
   have a sum of account values greater than 2,550,000
   otherwise set it to be `false`
  */
-var anyStatesInHigherStateSum = null;
-anyStatesInHigherStateSum = Object.entries(stateSums).some(topDollaCheck); // can run the same test but for "some" method
+var anyStatesInHigherStateSum = Object.entries(stateSums).some(topDollarCheck); // can run the same test but for "some" method
 
 module.exports = {
   hundredThousandairs: hundredThousandairs,
